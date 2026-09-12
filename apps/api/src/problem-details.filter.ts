c9d5258285c330a663ef57f75eb09b9d7f59532b
marketmaul56-gif <marketmaul56@gmail.com>
2026-09-12 17:52:57 +0700

    feat: map API exceptions to safe problem details

---
+++ apps/api/src/problem-details.filter.ts
@@ -0,0 +1,11 @@
+import { ArgumentsHost, Catch, type ExceptionFilter } from "@nestjs/common";
+import type { Response } from "express";
+import { writeProblem } from "./problem-details.ts";
+
+@Catch()
+export class ProblemDetailsFilter implements ExceptionFilter {
+  catch(error: unknown, host: ArgumentsHost): void {
+    const response = host.switchToHttp().getResponse<Response>();
+    writeProblem(response, error);
+  }
+}
