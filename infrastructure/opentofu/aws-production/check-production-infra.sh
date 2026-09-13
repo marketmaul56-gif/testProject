e6911704deee63ea2032096af6d4ed607bb0addb
marketmaul56-gif <marketmaul56@gmail.com>
2026-09-13 08:34:32 +0700

    test: strengthen PR.2 production security assertions

--- infrastructure/opentofu/aws-production/check-production-infra.sh
+++ infrastructure/opentofu/aws-production/check-production-infra.sh
@@ -24,8 +24,19 @@ require 'aws:SecureTransport' 'data.tf'
 require 'image_tag_mutability[[:space:]]*=[[:space:]]*"IMMUTABLE"' 'compute.tf'
 require 'http_tokens[[:space:]]*=[[:space:]]*"required"' 'verifier.tf'
 require 'associate_public_ip_address[[:space:]]*=[[:space:]]*false' 'verifier.tf'
+require 'target_group_arns[[:space:]]*=' 'verifier.tf'
+require 'latest_version' 'verifier.tf'
 require 'sha512sum --check --strict' 'templates/verifier-user-data.sh.tftpl'
 require 'VERIFIER_HOST_READY=1' 'templates/verifier-user-data.sh.tftpl'
+require 'token.actions.githubusercontent.com:aud' 'ci-federation.tf'
+require 'token.actions.githubusercontent.com:sub' 'ci-federation.tf'
+require 'repo:marketmaul56-gif@326277591/testProject@1360769907:ref:refs/heads/main' 'variables.tf'
+require 'iam:PassedToService' 'ci-federation.tf'
+
+if grep -Eq 'ignore_changes[[:space:]]*=[[:space:]]*\[[^]]*desired_capacity' "$ROOT/verifier.tf"; then
+  echo "verifier desired capacity must remain declarative for PR.3 activation" >&2
+  exit 1
+fi
 
 if grep -R -E 'AKIA[0-9A-Z]{16}|BEGIN (RSA |EC |OPENSSH )?PRIVATE KEY|aws_secret_access_key[[:space:]]*=' "$ROOT" --exclude='check-production-infra.sh'; then
   echo "possible static cloud credential/private key found in production infrastructure" >&2
